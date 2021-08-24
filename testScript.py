b3cea8d5f2861b09bd5d458a97448f0ddfc1019c
ZvikaPeer <zvika@dia-analysis.com>
2021-08-24 14:51:35 +0300

    Merge branch 'main' into myOtherBranch
    # Conflicts:
    #	testScript.py

--- testScript.py
+++ testScript.py
@@ -1,14 +1,1 @@
-print ("My Name is not lior")
-for i in range (5):
-    print ("main 1 ")
-
-print ("main 1 ")
-print ("MyName is Lior")
-
-print ("Andy duprain from  main 4")
-
-print ("This is my main ")
-
-print ("This is my main ") * 2
-
-print ("This is my main ") * haam
+main
